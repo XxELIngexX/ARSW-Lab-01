d9ba2d95da92baaaa9e8d9c9eccc9389f26eab76
unknown <hann.jang@labinfo.is.escuelaing.edu.co>
2024-08-15 09:57:59 -0500

    Avance

---
+++ src/main/java/edu/eci/arsw/blacklistvalidator/CheckSegment.java
@@ -0,0 +1,61 @@
+package edu.eci.arsw.blacklistvalidator;
+
+import java.util.ArrayList;
+import edu.eci.arsw.spamkeywordsdatasource.HostBlacklistsDataSourceFacade;
+
+
+public class CheckSegment implements Runnable{
+    
+    private ArrayList<Integer> blackListOcurrence;
+    private int start; 
+    private int end;
+    private String ipaddress;
+    public Thread hilo;
+    private HostBlacklistsDataSourceFacade skds;
+    private boolean finished;
+
+    public CheckSegment (int start, int end, String ipaddress){
+        hilo = new Thread(this);
+        this.start = start;
+        this.end = end;
+        this.ipaddress = ipaddress;
+        blackListOcurrence = new ArrayList<>();
+        skds=HostBlacklistsDataSourceFacade.getInstance();
+        finished = false;
+    }
+
+    @Override
+    public void run(){
+        for (int i = start; i<= end; i++){
+            if (skds.isInBlackListServer(i, ipaddress)){
+                blackListOcurrence.add(i);
+            }
+            synchronized (this) {
+                finished = true;
+            }
+        }
+    }    
+    
+    public synchronized ArrayList<Integer> getBlackListOcurrence(){
+        try {
+            while (!finished) {
+                wait ();
+            }
+        } catch (InterruptedException e){
+            System.out.println(hilo.getName()+"Interupted");
+        }
+        return blackListOcurrence;
+    }
+
+   
+    public synchronized int getOccurence(){
+        try {
+            while (!finished) {
+                wait();
+            }
+        }catch (InterruptedException e){
+            System.out.println(hilo.getName()+"Interupted");
+        }
+        return blackListOcurrence.size();
+    }
+}
